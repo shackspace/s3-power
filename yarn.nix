0687ab64d46a899e8e2b8d29ba3b8c299afece0a
makefu <github@syntax-fehler.de>
2019-10-31 14:16:15 +0100

    re-package with yarn2nix-moretea

--- yarn.nix
+++ yarn.nix
@@ -1,155 +1,138 @@
-{fetchurl, linkFarm}: rec {
+{ fetchurl, fetchgit, linkFarm, runCommandNoCC, gnutar }: rec {
   offline_cache = linkFarm "offline" packages;
   packages = [
-
     {
-      name = "aws-sdk-2.493.0.tgz";
+      name = "aws_sdk___aws_sdk_2.493.0.tgz";
       path = fetchurl {
-        name = "aws-sdk-2.493.0.tgz";
+        name = "aws_sdk___aws_sdk_2.493.0.tgz";
         url  = "https://registry.yarnpkg.com/aws-sdk/-/aws-sdk-2.493.0.tgz";
         sha1 = "2526208efc0fff91529c41c1d9f5cd15cdfcc937";
       };
     }
-
     {
-      name = "base64-js-1.3.0.tgz";
+      name = "base64_js___base64_js_1.3.0.tgz";
       path = fetchurl {
-        name = "base64-js-1.3.0.tgz";
+        name = "base64_js___base64_js_1.3.0.tgz";
         url  = "https://registry.yarnpkg.com/base64-js/-/base64-js-1.3.0.tgz";
         sha1 = "cab1e6118f051095e58b5281aea8c1cd22bfc0e3";
       };
     }
-
     {
-      name = "buffer-4.9.1.tgz";
+      name = "buffer___buffer_4.9.1.tgz";
       path = fetchurl {
-        name = "buffer-4.9.1.tgz";
+        name = "buffer___buffer_4.9.1.tgz";
         url  = "https://registry.yarnpkg.com/buffer/-/buffer-4.9.1.tgz";
         sha1 = "6d1bb601b07a4efced97094132093027c95bc298";
       };
     }
-
     {
-      name = "events-1.1.1.tgz";
+      name = "events___events_1.1.1.tgz";
       path = fetchurl {
-        name = "events-1.1.1.tgz";
+        name = "events___events_1.1.1.tgz";
         url  = "https://registry.yarnpkg.com/events/-/events-1.1.1.tgz";
         sha1 = "9ebdb7635ad099c70dcc4c2a1f5004288e8bd924";
       };
     }
-
     {
-      name = "ieee754-1.1.8.tgz";
+      name = "ieee754___ieee754_1.1.8.tgz";
       path = fetchurl {
-        name = "ieee754-1.1.8.tgz";
+        name = "ieee754___ieee754_1.1.8.tgz";
         url  = "https://registry.yarnpkg.com/ieee754/-/ieee754-1.1.8.tgz";
         sha1 = "be33d40ac10ef1926701f6f08a2d86fbfd1ad3e4";
       };
     }
-
     {
-      name = "ieee754-1.1.13.tgz";
+      name = "ieee754___ieee754_1.1.13.tgz";
       path = fetchurl {
-        name = "ieee754-1.1.13.tgz";
+        name = "ieee754___ieee754_1.1.13.tgz";
         url  = "https://registry.yarnpkg.com/ieee754/-/ieee754-1.1.13.tgz";
         sha1 = "ec168558e95aa181fd87d37f55c32bbcb6708b84";
       };
     }
-
     {
-      name = "isarray-1.0.0.tgz";
+      name = "isarray___isarray_1.0.0.tgz";
       path = fetchurl {
-        name = "isarray-1.0.0.tgz";
+        name = "isarray___isarray_1.0.0.tgz";
         url  = "https://registry.yarnpkg.com/isarray/-/isarray-1.0.0.tgz";
         sha1 = "bb935d48582cba168c06834957a54a3e07124f11";
       };
     }
-
     {
-      name = "jmespath-0.15.0.tgz";
+      name = "jmespath___jmespath_0.15.0.tgz";
       path = fetchurl {
-        name = "jmespath-0.15.0.tgz";
+        name = "jmespath___jmespath_0.15.0.tgz";
         url  = "https://registry.yarnpkg.com/jmespath/-/jmespath-0.15.0.tgz";
         sha1 = "a3f222a9aae9f966f5d27c796510e28091764217";
       };
     }
-
     {
-      name = "moment-2.24.0.tgz";
+      name = "moment___moment_2.24.0.tgz";
       path = fetchurl {
-        name = "moment-2.24.0.tgz";
+        name = "moment___moment_2.24.0.tgz";
         url  = "https://registry.yarnpkg.com/moment/-/moment-2.24.0.tgz";
         sha1 = "0d055d53f5052aa653c9f6eb68bb5d12bf5c2b5b";
       };
     }
-
     {
-      name = "punycode-1.3.2.tgz";
+      name = "punycode___punycode_1.3.2.tgz";
       path = fetchurl {
-        name = "punycode-1.3.2.tgz";
+        name = "punycode___punycode_1.3.2.tgz";
         url  = "https://registry.yarnpkg.com/punycode/-/punycode-1.3.2.tgz";
         sha1 = "9653a036fb7c1ee42342f2325cceefea3926c48d";
       };
     }
-
     {
-      name = "querystring-0.2.0.tgz";
+      name = "querystring___querystring_0.2.0.tgz";
       path = fetchurl {
-        name = "querystring-0.2.0.tgz";
+        name = "querystring___querystring_0.2.0.tgz";
         url  = "https://registry.yarnpkg.com/querystring/-/querystring-0.2.0.tgz";
         sha1 = "b209849203bb25df820da756e747005878521620";
       };
     }
-
     {
-      name = "sax-1.2.1.tgz";
+      name = "sax___sax_1.2.1.tgz";
       path = fetchurl {
-        name = "sax-1.2.1.tgz";
+        name = "sax___sax_1.2.1.tgz";
         url  = "https://registry.yarnpkg.com/sax/-/sax-1.2.1.tgz";
         sha1 = "7b8e656190b228e81a66aea748480d828cd2d37a";
       };
     }
-
     {
-      name = "sax-1.2.4.tgz";
+      name = "sax___sax_1.2.4.tgz";
       path = fetchurl {
-        name = "sax-1.2.4.tgz";
+        name = "sax___sax_1.2.4.tgz";
         url  = "https://registry.yarnpkg.com/sax/-/sax-1.2.4.tgz";
         sha1 = "2816234e2378bddc4e5354fab5caa895df7100d9";
       };
     }
-
     {
-      name = "url-0.10.3.tgz";
+      name = "url___url_0.10.3.tgz";
       path = fetchurl {
-        name = "url-0.10.3.tgz";
+        name = "url___url_0.10.3.tgz";
         url  = "https://registry.yarnpkg.com/url/-/url-0.10.3.tgz";
         sha1 = "021e4d9c7705f21bbf37d03ceb58767402774c64";
       };
     }
-
     {
-      name = "uuid-3.3.2.tgz";
+      name = "uuid___uuid_3.3.2.tgz";
       path = fetchurl {
-        name = "uuid-3.3.2.tgz";
+        name = "uuid___uuid_3.3.2.tgz";
         url  = "https://registry.yarnpkg.com/uuid/-/uuid-3.3.2.tgz";
         sha1 = "1b4af4955eb3077c501c23872fc6513811587131";
       };
     }
-
     {
-      name = "xml2js-0.4.19.tgz";
+      name = "xml2js___xml2js_0.4.19.tgz";
       path = fetchurl {
-        name = "xml2js-0.4.19.tgz";
+        name = "xml2js___xml2js_0.4.19.tgz";
         url  = "https://registry.yarnpkg.com/xml2js/-/xml2js-0.4.19.tgz";
         sha1 = "686c20f213209e94abf0d1bcf1efaa291c7827a7";
       };
     }
-
     {
-      name = "xmlbuilder-9.0.7.tgz";
+      name = "xmlbuilder___xmlbuilder_9.0.7.tgz";
       path = fetchurl {
-        name = "xmlbuilder-9.0.7.tgz";
+        name = "xmlbuilder___xmlbuilder_9.0.7.tgz";
         url  = "https://registry.yarnpkg.com/xmlbuilder/-/xmlbuilder-9.0.7.tgz";
         sha1 = "132ee63d2ec5565c557e20f4c22df9aca686b10d";
       };
